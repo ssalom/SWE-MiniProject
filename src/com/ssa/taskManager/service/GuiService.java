1a4ef43eef88682600a957e955a12b7595b637b1
Samuel Salomon <samuel.j.salomon@gmail.com>
2020-12-07 12:38:28 +0100

    Add localization and changed all outputs to this localization

--- src/com/ssa/taskManager/service/GuiService.java
+++ src/com/ssa/taskManager/service/GuiService.java
@@ -1,16 +1,46 @@
 package com.ssa.taskManager.service;
 
+import com.ssa.taskManager.utilities.Localization;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GuiService extends Application {
 
+
     @Override
     public void start(Stage primaryStage) {
-        
+        Scene scene = new Scene(createRootContainer(), 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(Localization.getLabels().getString("app-title"));
         primaryStage.show();
     }
+
+    public BorderPane createRootContainer () {
+        BorderPane root = new BorderPane();
+        root.setTop(createNavigation());
+        return root;
+    }
+
+    public MenuBar createNavigation () {
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu(Localization.getLabels().getString("file"));
+
+        MenuItem newTaskMenuItem = new MenuItem(Localization.getLabels().getString("add-task"));
+        MenuItem settingsMenuItem = new MenuItem(Localization.getLabels().getString("settings"));
+        MenuItem exitMenuItem = new MenuItem(Localization.getLabels().getString("exit"));
+        menu.getItems().addAll(newTaskMenuItem, settingsMenuItem, exitMenuItem);
+
+        menuBar.getMenus().add(menu);
+
+        return menuBar;
+    }
+
+
 }
